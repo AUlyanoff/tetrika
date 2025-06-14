31119909eb02e848c05b4b0e408b13db1130d53c
Artem Ivochkin <ivochkinartem@gmail.com>
2025-06-14 19:09:03 +0300

    альфа проверки типов

--- src/main.py
+++ src/main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+# -*- coding: utf-8 -*-
+from serv.type_checking.decorator import strict
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+@strict
+def sum_two_int(a: int, b: int) -> int:
+    """Суммирование целых аргументов"""
+    return a + b
 
+@strict
+def sum_two_float(a: float, b: float) -> float:
+    """Суммирование целых аргументов"""
+    return a + b
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # print(sum_two_int(1, 2))
+    print(sum_two_float(3.14, 1.))
+    pass
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+
--- src/serv/type_checking/decorator.py
+++ src/serv/type_checking/decorator.py
@@ -1,2 +1,21 @@
 # -*- coding: utf-8 -*-
+from functools import wraps
+# import logging
 
+# logger = logging.getLogger(__name__)
+
+def strict(func):
+    """Проверка фактических типов данных аннотированным"""
+    @wraps(func)
+    def wrap(*args, **kwargs):
+        print(args)
+        for k, v in func.__annotations__.items():
+            print(f'{k} = {v.__name__}')
+
+        for v, t in zip(args, func.__annotations__.values()):
+            print(f'{v} = {t.__name__}')
+            if type(v) != t: raise TypeError
+
+        return func(*args, **kwargs)
+
+    return wrap
