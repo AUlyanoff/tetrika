dd287963c0b781771c877762e3acbc93538e1e4f
Artem Ivochkin <ivochkinartem@gmail.com>
2025-06-15 13:54:04 +0300

    вики бета

--- src/wiki_parsing/animal_counting.py
+++ src/wiki_parsing/animal_counting.py
@@ -6,10 +6,10 @@ import wikipediaapi
 logger = logging.getLogger(__name__)
 
 
-def get_beasts(article: str):
+def get_beasts(article: str, agent: str = 'Tetrika (junir@tetrika.com)'):
+    """Подсчёт количества животных на каждую букву алфавита"""
 
-    wiki = wikipediaapi.Wikipedia(user_agent='Tetrika (ceo@tetrika.com)', language="ru",
-                                  extract_format=wikipediaapi.ExtractFormat.WIKI)
+    wiki = wikipediaapi.Wikipedia(user_agent=agent, language="ru")
     page = wiki.page(article)
     if not page.exists():
         logger.error(f"Wikipedia page '{article}' not found")
@@ -21,28 +21,23 @@ def get_beasts(article: str):
 
     with open('beasts.csv', 'w', encoding='utf-8') as f:
         f.write("Количество животных на каждую букву алфавита:\n")
-
-        """
-        текущая буква = первая буква первого животного
-        счётчик = 0
-        Перебираем список животных
-            если первая буква не изменилась
-                счётчик += 1
-            иначе
-                текущую букву и счётчик дописываем в файл
-                текущая буква = первая буква
-                счётчик = 1
-        текущую букву и счётчик дописываем в файл
-        """
-        count = 0
-        current_letter = animal_list[0][0]
+        count = total = 0
+        current_letter = animal_list[0][0]                  # текущая буква = первая буква первого животного
         for animal in animal_list:
-            first_letter = animal[0]
-            if animal[0] == current_letter:
+            if animal[0] == current_letter:                 # если первая буква не изменилась
                 count += 1
             else:
-                f.write(f"{current_letter}: {count}\n")
-                current_letter = animal[0]
-                count = 1
-        f.write(f"{current_letter}: {count}\n")
+                f.write(f"{current_letter}: {count}\n")     # дописываем текущую букву и счётчик повторений
+                logger.debug(f"'{current_letter}' completed: {count}")
+                total += count
+                current_letter = animal[0]                  # текущая буква = первая буква текущего животного
+                count = 1                                   # потому что первая буква текущего животного изменилась
+        f.write(f"{current_letter}: {count}\n")             # дописываем последнюю букву и счётчик повторений
+        logger.debug(f"'{current_letter}' completed: {count}")
+        total += count
 
+        if animal_list.__len__() != total:
+            logger.warning('Total animals number not equal animals list length')
+
+    logger.debug(f"Counting completed: {total}")
+    return True
