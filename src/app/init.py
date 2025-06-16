76eb98f02f36cd925194b8c5b2a9a7ae53cdf62b
Artem Ivochkin <ivochkinartem@gmail.com>
2025-06-16 12:48:18 +0300

    совершенству нет предела

--- src/app/init.py
+++ src/app/init.py
@@ -3,6 +3,7 @@ import logging
 
 tetrika = "'Онлайн-школa Тетрика' test task started"
 
+# настройка логирования
 logger = logging.getLogger(__name__)
 logging.basicConfig(force=True, level=logging.DEBUG, datefmt='%H:%M:%S',
                     format='%(asctime)s %(levelname).1s: %(filename)s/%(funcName)s/%(lineno)d: %(message)s')
